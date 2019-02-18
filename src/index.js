39672298aa3e879bd68803bd418f2d0965c5b092
DeePenguin <_deadpenguin_@mail.ru>
2019-02-19 01:32:08 +0300

    Add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let innerbase = base;
+  let multiplier = 2;
+  const simpleMultipliers = new Map();
+  
+  while (multiplier <= innerbase){
+    const remainder = innerbase % multiplier;
+    let powCounter = 0;
+    if (!remainder){
+      while (!(innerbase % multiplier)){
+        powCounter++;
+        innerbase /= multiplier;
+      }
+      simpleMultipliers.set(multiplier,powCounter);
+    } else multiplier++;
+  }
+
+  const dividersCounter = (number, multipliers) => {
+    let zeroes = Number.MAX_SAFE_INTEGER;
+    for (let [multiplier, pow] of multipliers){
+      let counter = 0;
+      let i = 1; 
+      do {
+        denominator = Math.pow(multiplier, i);
+        counter += Math.trunc(number / denominator);
+        i++;
+      } while (number / denominator >= 1);
+      zeroes = Math.min(zeroes, Math.trunc(counter / pow));
+    }
+    return zeroes;
+  };
+  return dividersCounter(number, simpleMultipliers);
 }
